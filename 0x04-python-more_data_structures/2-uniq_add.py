3139f048013d8fa20c3df2635007f7acb9c19a8b
khabeko <khabeko@outlook.com>
2022-12-07 06:42:25 -0800

    corrections for tasks

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-
-
 def uniq_add(my_list=[]):
-    result = 0
-    for x in set(my_list):
-        result += x
-    return
+    uniq_list = set(my_list)
+    num = 0
+
+    for i in uniq_list:
+        num += i
+
+    return (num)
